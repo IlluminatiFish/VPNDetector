76c4450d5d5f5e5080ef0589fb97fd92cc7e4343
IlluminatiFish <45714340+IlluminatiFish@users.noreply.github.com>
2021-11-21 21:02:53 +0000

    Update VPNDetector.py

--- VPNDetector.py
+++ VPNDetector.py
@@ -1,51 +1,20 @@
-#
-# VPNDetector v1.4
-#
-# This script should be used primarly for
-# singular IP lookups 
-#
-# Coded by IlluminatiFish
-#
-
 from bs4 import BeautifulSoup
 import requests, re
 
-spans = []
-
-def cleanHTML(raw_html):
-    cleaner = re.compile('<.*?>')
-    cleaned = re.sub(cleaner, '', str(raw_html))
-    return cleaned.strip()
-
-def clearParagraph(raw_paragraph):
-    return str(raw_paragraph).split('is part of ')[1].split('.')[0]
-
-def getProvider(ip):
-    spans.clear()
-    
+def get_vpn_provider(ip):
+  
     request = requests.get('https://spur.us/context/{}'.format(ip))
-    
     soup = BeautifulSoup(request.text, 'html.parser')
     
-    for span in soup.find_all('span'):
-        spans.append(span)
-
-    if len(spans) > 2:
-
-        if cleanHTML(spans[2]) == "Not Anonymous" or cleanHTML(spans[2]) == "Possibly Anonymous" or cleanHTML(spans[2]) == "Likely Anonymous":
-            return None
-
-        
-        if cleanHTML(spans[2]) == '': #Catches Proxies
-            paras = soup.find_all('p')
-            return clearParagraph(paras[0]).upper()
-        
-        return cleanHTML(spans[2]).upper()
+    for meta_tag in soup.find_all('meta'):
+        search = re.search('(.*) \( (.*) \) IP Context', str(meta_tag.get('content')))
+        if search:
+            return search.group(2)
 
 
 ip = input("[+] IP: ")
-result = getProvider(ip)
-print('Provider:',result, '~~~ IP:',ip)
+result = get_vpn_provider(ip)
+print('Provider:', result)
 
 
 
